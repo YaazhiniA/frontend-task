f705e7c3764c200a62fb0c35f31a43461a330360
Yaazhini A <60349341+YaazhiniA@users.noreply.github.com>
2021-03-24 13:12:31 +0530

    updated Recommended Videos

---
+++ src/RecommendedVideos/RecommendedVideos.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import VideoCard from './VideoCard';
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (        
+           <div className="recommendedVideos">
+              <h2>Recommended</h2>
+              <div className="recommendedVideos__videos">
+                  <VideoCard 
+                    title="15 Times Cristiano Ronaldo Showed Who Is The Boss"
+                    views="8.7M Views "
+                    timestamp=" 1 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngAJ17J53bJRMP9SNNPjUOccYM_F6SdxTe-5lu8=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Ronnie7M"
+                    image="https://img.youtube.com/vi/TYXQQ2uO5pk/maxresdefault.jpg" 
+                  />
+                  <VideoCard 
+                    title="Dueling Club - Harry Potter"
+                    views="741K Views "
+                    timestamp=" 2 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnhiSMA6zPSfLfUGQj9PA6MJYJy99RhAUnWllcuIhg=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Top Moments "
+                    image="https://img.youtube.com/vi/QSiynI-fjYE/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="Taiwanese Street Food Handmade Pizza"
+                    views="47M Views "
+                    timestamp=" 1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngdsC9Qn1Px4nyiCYZwvv3V17XPC_VT9fPZJaJY=s68-c-k-c0x00ffffff-no-rj"
+                    channel="DancingBacons"
+                    image="https://img.youtube.com/vi/PCAwJs51D0k/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="At the Hospital | Funny Episodes | Classic Mr Bean"
+                    views="72M Views "
+                    timestamp=" 1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngkxXitqc_jzNI1_d1CsW3lY6kyFEL67kgEvw9JcA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Classic Mr Bean"
+                    image="https://img.youtube.com/vi/-72P_EFphSc/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="Horrible experiment hydraulic press vs."
+                    views="2.5M Views "
+                    timestamp=" 1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnigSKccuaitBy0GAFaz9EHv2idmWE4WckfekVfc=s68-c-k-c0x00ffffff-no-rj"
+                    channel="principle Mechanical"
+                    image="https://img.youtube.com/vi/iHGTie5xsr4/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="FAQs surrounding COVID 19 vaccine introduction..."
+                    views="85K Views"
+                    timestamp="2 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwni4yhErLqNtIboW-Twz_Fihinnh6rPX0Hp-iil-TA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Ministry of Health & Family Welfare"
+                    image="https://img.youtube.com/vi/jL3Fh8yJQKo/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="Funny Dogs Reaction To Vet You'll LAUGH more than..."
+                    views="8.7M Views "
+                    timestamp=" 2 monthS ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngfVFHBPlRemHUzbM1TkZ4ZmZ1M-8bZr1PB8NXV=s68-c-k-c0x00ffffff-no-rj"
+                    channel="MAI PM"
+                    image="https://img.youtube.com/vi/WOCVTLnq9wM/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="Baby Cats | Cute And Funny cat Videos Compilation #60..."
+                    views="9.8M Views "
+                    timestamp=" 4 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnixJamRJW3KQ_wMfAv6nSYKjCnwd4hCHb8UG892=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Cute Cats Land"
+                    image="https://img.youtube.com/vi/uHKfrz65KSU/maxresdefault.jpg"
+                  />
+                  <VideoCard 
+                    title="Space Saving Furniture Ideas for Small Apartments"
+                    views="9M Views "
+                    timestamp=" 7 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniBCh9RxLnMgrOOWyatm73Mg7I5irxRCBVvwhdm2Q=s68-c-k-c0x00ffffff-no-rj"
+                    channel="TechFreeze"
+                    image="https://img.youtube.com/vi/u5RvDg8uFv0/maxresdefault.jpg"
+                  />
+              </div>
+           </div>
+
+    );
+}
+
+export default RecommendedVideos
