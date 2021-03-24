288fd03e16d2d399d19d63bb391f769e0dbabb5a
Yaazhini A <60349341+YaazhiniA@users.noreply.github.com>
2021-03-24 10:17:56 +0530

    updated Recommended Videos

---
+++ src/RecommendedVideos/RecommendedVideos.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import VideoCard from './VideoCard';
+import './RecommendedVideos.css';
+
+function RecommendedVideos() {
+    return (        
+           <div className="recommendedVideos">
+              <h2>Recommended</h2>
+              <div className="recommendedVideos__videos">
+                  <VideoCard 
+                    title="15 Times Cristiano Ronaldo Showed Who Is The Boss"
+                    views="8.7M Views "
+                    timestamp=" 1 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngAJ17J53bJRMP9SNNPjUOccYM_F6SdxTe-5lu8=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Ronnie7M"
+                    image="https://i.ytimg.com/an_webp/4BouXgKewds/mqdefault_6s_480x270.webp?du=3000&sqp=CJWS54IG&rs=AOn4CLBH2xxNZIBeghpnFda9Gh6t3yDVhg" 
+                  />
+                  <VideoCard 
+                    title="Dueling Club - Harry Potter"
+                    views="741K Views "
+                    timestamp=" 2 years ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnhiSMA6zPSfLfUGQj9PA6MJYJy99RhAUnWllcuIhg=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Top Moments "
+                    image="https://i.ytimg.com/an_webp/NLVP1DxT3ns/mqdefault_6s.webp?du=3000&sqp=CNLL54IG&rs=AOn4CLCgXWimkjs_yAW1XuVnIL2VYk_lJQ"
+                  />
+                  <VideoCard 
+                    title="Taiwanese Street Food Handmade Pizza"
+                    views="47M Views "
+                    timestamp=" 1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngdsC9Qn1Px4nyiCYZwvv3V17XPC_VT9fPZJaJY=s68-c-k-c0x00ffffff-no-rj"
+                    channel="DancingBacons"
+                    image="https://i.ytimg.com/an_webp/qGRvHSAmN4o/mqdefault_6s_480x270.webp?du=3000&sqp=CILU54IG&rs=AOn4CLCNtvRqWeW1fR38jX9bSeyXXk2gAw"
+                  />
+                  <VideoCard 
+                    title="At the Hospital | Funny Episodes | Classic Mr Bean"
+                    views="72M Views "
+                    timestamp=" 1 year ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngkxXitqc_jzNI1_d1CsW3lY6kyFEL67kgEvw9JcA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Classic Mr Bean"
+                    image="https://i.ytimg.com/an_webp/-72P_EFphSc/mqdefault_6s_480x270.webp?du=3000&sqp=CMCe54IG&rs=AOn4CLAjPnoCVuTpr-pHQTB1baS0Slj1mQ"
+                  />
+                  <VideoCard 
+                    title="Horrible experiment hydraulic press vs."
+                    views="2.5M Views "
+                    timestamp=" 1 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnigSKccuaitBy0GAFaz9EHv2idmWE4WckfekVfc=s68-c-k-c0x00ffffff-no-rj"
+                    channel="principle Mechanical"
+                    image="https://i.ytimg.com/an_webp/iHGTie5xsr4/mqdefault_6s.webp?du=3000&sqp=CLzA54IG&rs=AOn4CLDd9n7xfQ-vJ0UwEa2Vw9tUrrYZQw"
+                  />
+                  <VideoCard 
+                    title="FAQs surrounding COVID 19 vaccine introduction..."
+                    views="85K Views"
+                    timestamp="2 month ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwni4yhErLqNtIboW-Twz_Fihinnh6rPX0Hp-iil-TA=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Ministry of Health & Family Welfare"
+                    image="https://i.ytimg.com/an_webp/krneH_d9nd8/mqdefault_6s.webp?du=3000&sqp=CJKw54IG&rs=AOn4CLCjYgSVhOpW13_hydu9dAdYsQiTqA"
+                  />
+                  <VideoCard 
+                    title="Funny Dogs Reaction To Vet You'll LAUGH more than..."
+                    views="8.7M Views "
+                    timestamp=" 2 monthS ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwngfVFHBPlRemHUzbM1TkZ4ZmZ1M-8bZr1PB8NXV=s68-c-k-c0x00ffffff-no-rj"
+                    channel="MAI PM"
+                    image="https://i.ytimg.com/an_webp/WOCVTLnq9wM/mqdefault_6s.webp?du=3000&sqp=CLnb54IG&rs=AOn4CLCyzSKGYDbbZBOI7hF5tLCkTB8-EA"
+                  />
+                  <VideoCard 
+                    title="Baby Cats | Cute And Funny cat Videos Compilation #60..."
+                    views="9.8M Views "
+                    timestamp=" 4 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwnixJamRJW3KQ_wMfAv6nSYKjCnwd4hCHb8UG892=s68-c-k-c0x00ffffff-no-rj"
+                    channel="Cute Cats Land"
+                    image="https://i.ytimg.com/an_webp/DMqqv30eL7w/mqdefault_6s.webp?du=3000&sqp=CMS354IG&rs=AOn4CLDOfTUUFTmhciLhq6zL_5kqzwCddw"
+                  />
+                  <VideoCard 
+                    title="Space Saving Furniture Ideas for Small Apartments"
+                    views="9M Views "
+                    timestamp=" 7 months ago"
+                    channelImage="https://yt3.ggpht.com/ytc/AAUvwniBCh9RxLnMgrOOWyatm73Mg7I5irxRCBVvwhdm2Q=s68-c-k-c0x00ffffff-no-rj"
+                    channel="TechFreeze"
+                    image="https://i.ytimg.com/an_webp/u5RvDg8uFv0/mqdefault_6s.webp?du=3000&sqp=CJPL54IG&rs=AOn4CLBFKVqlzOM2kA2p-n5NIhgvyWqcdQ"
+                  />
+              </div>
+           </div>
+
+    );
+}
+
+export default RecommendedVideos
